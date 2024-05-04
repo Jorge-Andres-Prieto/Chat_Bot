0478cbd0501c656e67acd80073b0e3c17166fee4
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-05-04 12:20:57 -0500

    14

--- front.py
+++ front.py
@@ -1,61 +1,49 @@
 import streamlit as st
 from chat_bot import predict_class, get_response, intents
 
-# Configura el título y las configuraciones iniciales de la aplicación de Streamlit
-st.set_page_config(layout="wide")  # Ocupa toda la pantalla
+# Configura el título de la aplicación de Streamlit
 st.title('🤖 UNalBot')
 
-# Sidebar para futuras opciones
-sidebar = st.sidebar
-sidebar.title("Opciones")
+# Inicializa el estado de la sesión para almacenar mensajes si aún no está hecho
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "first_message" not in st.session_state:
+    st.session_state.first_message = True
 
-# Divide la pantalla en dos columnas
-col1, col2 = st.columns(2)
+# Sidebar para preguntas frecuentes
+st.sidebar.title("Preguntas Frecuentes")
+st.sidebar.write("1. ¿Cómo puedo contactar a soporte?")
+st.sidebar.write("R: Puedes contactarnos vía email en soporte@unal.edu.co")
+st.sidebar.write("2. ¿Dónde encuentro información sobre matrículas?")
+st.sidebar.write("R: Toda la información sobre matrículas está disponible en nuestro portal web.")
 
-# Columna 1: Chat Bot
-with col1:
-    # Inicializa el estado de la sesión para almacenar mensajes si aún no está hecho
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    if "first_message" not in st.session_state:
-        st.session_state.first_message = True
+# Muestra los mensajes guardados en el estado de la sesión
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Muestra los mensajes guardados en el estado de la sesión
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# Envía el primer mensaje del asistente si es la primera interacción
+if st.session_state.first_message:
+    with st.chat_message("assistant"):
+        st.markdown("Hola, ¿cómo puedo ayudarte?")
+    st.session_state.messages.append({
+        "role": "assistant", "content": "Hola, ¿cómo puedo ayudarte?"
+    })
+    st.session_state.first_message = False
 
-    # Envía el primer mensaje del asistente si es la primera interacción
-    if st.session_state.first_message:
-        with st.chat_message("assistant"):
-            st.markdown("Hola, ¿cómo puedo ayudarte?")
-        st.session_state.messages.append({
-            "role": "assistant", "content": "Hola, ¿cómo puedo ayudarte?"
-        })
-        st.session_state.first_message = False
+# Captura y maneja la entrada del usuario
+prompt = st.chat_input("¿cómo puedo ayudarte?")
+if prompt:
+    # Agrega y muestra el mensaje del usuario
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Formulario para la entrada del usuario
-    with st.form("chat_form"):
-        prompt = st.text_input("¿Cómo puedo ayudarte?", key="input")
+    # Procesa la entrada del usuario usando el modelo de IA
+    intents_list = predict_class(prompt)
+    response = get_response(intents_list, intents)
 
-        # Botón de envío del formulario
-        submitted = st.form_submit_button("Enviar")
-        if submitted and prompt:
-            # Agrega y muestra el mensaje del usuario
-            with st.chat_message("user"):
-                st.markdown(prompt)
-            st.session_state.messages.append({"role": "user", "content": prompt})
-
-            # Procesa la entrada del usuario usando el modelo de IA
-            intents_list = predict_class(prompt)
-            response = get_response(intents_list, intents)
-
-            # Envía y muestra la respuesta del asistente
-            with st.chat_message("assistant"):
-                st.markdown(response)
-            st.session_state.messages.append({"role": "assistant", "content": response})
-
-# Columna 2: Preguntas Frecuentes
-with col2:
-    st.header("Preguntas Frecuentes")
-    st.write("Aquí irán las preguntas frecuentes y sus respuestas.")
+    # Envía y muestra la respuesta del asistente
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
